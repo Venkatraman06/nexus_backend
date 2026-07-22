d882d2f3fb2c0058d16fb5d78eaea25438a1e28a
Madhan2006p <madhanp28072006@gmail.com>
2026-07-22 15:45:34 +0530

    fix(master): restore serializers import in apps/master/serializers.py

--- apps/master/serializers.py
+++ apps/master/serializers.py
@@ -1,3 +1,4 @@
+from rest_framework import serializers
 from .models import (
     Designation, Department, Location, Grade, EmploymentType,
     ShiftCategory, RateCard, ClientCategory, BusinessType, BillingType,
